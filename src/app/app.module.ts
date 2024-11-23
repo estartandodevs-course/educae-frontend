05eeae1711908a4b4d802dbe6f293113cfe2b089
Bruno spanier <brunospanier@hotmail.com>
2024-11-23 16:09:53 -0300

    feat(nav-bar fixed): nav and home fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AppNavbarComponent } from './app-navbar/app-navbar.component';
-import { HomeComponent } from './home/home.component';
+import { AppNavbarComponent } from './core/components/app-navbar/app-navbar.component';
+import { HomeComponent } from './Features/home/components/home-page/home.component';
 
 @NgModule({
   declarations: [AppComponent, AppNavbarComponent, HomeComponent],
---
+++ src/app/core/components/app-navbar/app-navbar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-navbar',
+  templateUrl: './app-navbar.component.html',
+  styleUrl: './app-navbar.component.scss',
+})
+export class AppNavbarComponent {}
